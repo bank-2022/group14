5dd1da0d3034c30c830a5139892a9fa6d6a7af23
robnikul <robnikul@gmail.com>
2022-04-13 12:24:58 +0300

    RESTapi
    Rest api tehty, bcryptistä en varma toimiiko, phpmyadmin näyttää nullia hashin kohdalla.

--- api/app.js
+++ api/app.js
@@ -2,19 +2,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const basicAuth = require('express-basic-auth');
+const helmet = require('helmet');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+const asiakasRouter = require('./routes/asiakas');
+const loginRouter = require('./routes/login');
+const tiliRouter = require('./routes/tili');
+const korttiRouter = require('./routes/kortti');
+const tilitapahtumatRouter = require('./routes/tilitapahtumat');
+const userRouter = require('./routes/user');
+
 
 var app = express();
 
+//router.use(basicAuth({users: { 'admin': '1234' }}))
+
+app.use(helmet());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tili', tiliRouter)
+app.use('/asiakas', asiakasRouter)
+app.use('/login', loginRouter)
+app.use('/kortti', korttiRouter)
+app.use('/tilitapahtumat', tilitapahtumatRouter)
+app.use('/user', userRouter)
+
+
+app.use(basicAuth({users: { 'admin': '1234' }}))
 
 module.exports = app;
---
+++ api/models/kortti_model.js
@@ -0,0 +1,35 @@
+const db = require('../database');
+const bcrypt = require('bcryptjs');
+
+const saltRounds=10;
+
+const kortti = {
+  getById: function(id, callback) {
+    return db.query('select * from kortti where idKortti=?', [id], callback);
+  },
+  getAll: function(callback) {
+    return db.query('select * from kortti', callback);
+  },
+  add: function(kortti, callback) {
+      bcrypt.hash(kortti.pin, saltRounds, function(err, hashedpin){
+        return db.query(
+            'insert into kortti (idKortti,korttinumero,pin,idAsiakas,idTili) values(?,?,?,?,?)',
+            [kortti.idKortti, kortti.korttinumero, hashedpin, kortti.idAsiakas, kortti.idTili],
+            callback
+          );          
+      })
+  },
+  delete: function(id, callback) {
+    return db.query('delete from kortti where idKortti=?', [id], callback);
+  },
+  update: function(id, kortti, callback) {
+    bcrypt.hash(kortti.pin, saltRounds, function(err, hash){
+      return db.query(
+          'update kortti set idKortti=?, korttinumero=?, pin=?,idAsiakas=?, idTili=? WHERE idKortti=?',
+          [kortti.idKortti, kortti.korttinumero, hash, kortti.idAsiakas, kortti.idTili, id],
+          callback
+        );          
+    })
+}
+};
+module.exports = kortti;
